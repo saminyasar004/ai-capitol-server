ec4ec8ecf1167e7f5ea9440a682e71f3334be653
Samin Yasar <yasarsamin57@gmail.com>
2025-07-28 13:29:53 +0600

    dev: production database connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,31 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
-import { EmailService } from './email/email.service';
-import { OtpService } from './otp/otp.service';
-import { AiModule } from './ai/ai.module';
-import { CategoryModule } from './category/category.module';
+import { EmailService } from "./email/email.service";
+import { OtpService } from "./otp/otp.service";
+import { AiModule } from "./ai/ai.module";
+import { CategoryModule } from "./category/category.module";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { dbConnectionString } from "./config/dotenv.config";
 
 @Module({
-  imports: [AuthModule, AiModule, CategoryModule],
+  imports: [
+    AuthModule,
+    AiModule,
+    CategoryModule,
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      uri: dbConnectionString,
+      models: [__dirname + "/**/*.model{.ts,.js}"],
+      autoLoadModels: true,
+      synchronize: false, // Set to false in production
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false, // Required for Supabase
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, EmailService, OtpService],
 })
